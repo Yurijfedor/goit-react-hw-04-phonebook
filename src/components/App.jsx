afbcfd6f8f5a197fa38888b9ace21427cc8c0770
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-11-15 22:01:14 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,20 @@ import { Filter } from './phonebook/filter';
 const LS_KEY = 'my contacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const savedContacts = JSON.parse(localStorage.getItem(LS_KEY));
-    setContacts([...savedContacts]);
+    if (savedContacts) {
+      setContacts([...savedContacts]);
+    }
+    return;
   }, []);
 
   useEffect(() => {
